fa98b21c0e75bf75a5e268f86e3409daad68837b
Alex Tsvetanov <alex.tsvetanov@chaos.com>
2024-07-17 16:37:56 +0300

    Update schedule_messages.js
    Update schedule_messages.js
    
    Update schedule_messages.js
    
    Try upgrading the versions

--- commands/schedule_messages.js
+++ commands/schedule_messages.js
@@ -9,25 +9,79 @@ class ScheduleMessagesCommand {
 		name: 'schedule_message',
 		description: 'Schedule a message to be sent in the future or even many times in the future.',
 		type: applicationCommandTypes.CHAT_SLASH_COMMAND,
+		options: [
+			{
+				name: "message",
+				description: "Schedule just a simple text",
+				type: 1
+			},
+			{
+				name: "poll",
+				description: "Schedule a whole poll",
+				type: 1
+			}
+		]
 	};
 
 	static async execute(res, data) {
+		console.log(data);
+		const subcommand = data.options[0].name;
 		// Send a message into the channel where command was triggered from
-		return {
-			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-			data: {
-				content: 'Hello there',
-				poll: {
-					question: { text: "text?" },
-					answers: [
-						{answer_id: 1, poll_media: { text: "test1" }},
-						{answer_id: 2, poll_media: { text: "test2" }},
-					],
-					allow_multiselect: true
+		if (subcommand == this.command_data.options[0].name)
+			return {
+				type: InteractionResponseType.MODAL,
+				data: {
+					custom_id: "messagemodal",
+					title: 'Time to setup the message',
+					components: [
+						{
+							custom_id: 'message_content',
+							type: componentTypes.TextInput,
+							style: 2, // Paragraph
+							label: 'Your message',
+							required: true,
+							placeholder: 'Official match this weekend!'
+						},
+					]
 				}
-			}
-		};
+			};
+		else
+			return {
+				type: InteractionResponseType.MODAL,
+				data: {
+					custom_id: "pollmodal",
+					title: 'Time to setup the poll',
+					components: [
+						{
+							custom_id: 'pool_question',
+							type: componentTypes.TextInput,
+							style: 2, // Paragraph
+							label: 'Your question',
+							required: true,
+							placeholder: 'When are you available next week?'
+						},
+						{
+							custom_id: 'pool_answers',
+							type: componentTypes.TextInput,
+							style: 2, // Paragraph
+							label: 'Your options. One per line.',
+							required: true,
+							placeholder: 'Monday evening\nWednesday evening\nFriday evening'
+						},
+					]
+				}
+			};
 	}
 }
 
 export default ScheduleMessagesCommand;
+
+/*,
+					poll: {
+						question: { text: "text?" },
+						answers: [
+							{answer_id: 1, poll_media: { text: "test1" }},
+							{answer_id: 2, poll_media: { text: "test2" }},
+						],
+						allow_multiselect: true
+					}*/
